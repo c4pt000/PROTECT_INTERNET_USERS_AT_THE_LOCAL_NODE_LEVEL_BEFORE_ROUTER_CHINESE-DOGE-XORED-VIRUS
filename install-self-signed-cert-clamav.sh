5974f7111b7473118838cdacf8f70ca2336d138f
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-06-11 14:58:39 -0400

    Update install-self-signed-cert-clamav.sh

--- install-self-signed-cert-clamav.sh
+++ install-self-signed-cert-clamav.sh
@@ -4,8 +4,39 @@
 # wont work due to not having the self signed cert for squidguard clamav virus scanning
 # cert must also be import to each browser on a LAN firefox or chrome as a CA import
 
-sudo yum install sudo wget -y
-apt-get install sudo wget -y
-wget https://github.com/c4pt000/XORED-defense-photochain-project/raw/main/ECDSA.crt
+#fedora
+echo "
+-----BEGIN CERTIFICATE-----
+MIIBrzCCAVSgAwIBAgIBADAKBggqhkjOPQQDBDAdMQ4wDAYDVQQDEwVFQ0RTQTEL
+MAkGA1UEBhMCVVMwHhcNMjEwMzIzMTI0NzI2WhcNMzEwMzIxMTI0NzI2WjAdMQ4w
+DAYDVQQDEwVFQ0RTQTELMAkGA1UEBhMCVVMwWTATBgcqhkjOPQIBBggqhkjOPQMB
+BwNCAATE186oMbvokUyrrvgUYSJXG9+M5EUNVz0cydzn4x5TdHkuMk5/vlweKBhr
+TV0CKZN0rUs1QWklhX/34e/tQOp1o4GEMIGBMB0GA1UdDgQWBBQeBQCziCMHz6oa
+2K/TkLAGt7pHnzBFBgNVHSMEPjA8gBQeBQCziCMHz6oa2K/TkLAGt7pHn6EhpB8w
+HTEOMAwGA1UEAxMFRUNEU0ExCzAJBgNVBAYTAlVTggEAMAwGA1UdEwQFMAMBAf8w
+CwYDVR0PBAQDAgEGMAoGCCqGSM49BAMEA0kAMEYCIQC1MJQfvBpqzONLKNuCuxng
+3Y7e+PuMnLiW+nHnkqdofAIhANYqlKFybVs4fBT4qxut+pCpR9HCUKmarDHMvL19
+ot2P
+-----END CERTIFICATE-----
+" > ECDSA.crt
 sudo cp ECDSA.crt /usr/share/pki/ca-trust-source/anchors/
 sudo update-ca-trust
+
+
+## ubuntu
+echo "
+-----BEGIN CERTIFICATE-----
+MIIBrzCCAVSgAwIBAgIBADAKBggqhkjOPQQDBDAdMQ4wDAYDVQQDEwVFQ0RTQTEL
+MAkGA1UEBhMCVVMwHhcNMjEwMzIzMTI0NzI2WhcNMzEwMzIxMTI0NzI2WjAdMQ4w
+DAYDVQQDEwVFQ0RTQTELMAkGA1UEBhMCVVMwWTATBgcqhkjOPQIBBggqhkjOPQMB
+BwNCAATE186oMbvokUyrrvgUYSJXG9+M5EUNVz0cydzn4x5TdHkuMk5/vlweKBhr
+TV0CKZN0rUs1QWklhX/34e/tQOp1o4GEMIGBMB0GA1UdDgQWBBQeBQCziCMHz6oa
+2K/TkLAGt7pHnzBFBgNVHSMEPjA8gBQeBQCziCMHz6oa2K/TkLAGt7pHn6EhpB8w
+HTEOMAwGA1UEAxMFRUNEU0ExCzAJBgNVBAYTAlVTggEAMAwGA1UdEwQFMAMBAf8w
+CwYDVR0PBAQDAgEGMAoGCCqGSM49BAMEA0kAMEYCIQC1MJQfvBpqzONLKNuCuxng
+3Y7e+PuMnLiW+nHnkqdofAIhANYqlKFybVs4fBT4qxut+pCpR9HCUKmarDHMvL19
+ot2P
+-----END CERTIFICATE-----
+" > ECDSA.crt
+sudo cp ECDSA.crt /usr/local/share/ca-certificates/
+sudo update-ca-certificates 
